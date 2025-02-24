cf45163fc7c452916713a84f61183ba9592877ed
Renan <renandiutida@gmail.com>
2025-02-24 14:05:21 -0300

    Criando condicoes com if/else e while, alem de criar toda a logica do jogo do app.js

--- app.js
+++ app.js
@@ -2,14 +2,27 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-// Se chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (5)');
-} else {
-    alert('Você errou :(');
+// Enquanto não for igual ao N.S.
+while (chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // Se chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} tentativas!`);
+    } else {
+        if (chute < numeroSecreto) {
+            alert(`Você errou! O número secreto é maior que ${chute}`);   
+        } else{
+            alert(`Você errou! O número secreto é menor que ${chute}`);
+        }
+        tentativas++;
+    }
 }
+
 
 
 
